cda2f8a78bcd543a1b00df128bdb44a40371b902
Kapotnybanana1858 <90246832+Kapotnybanana1858@users.noreply.github.com>
2021-12-24 11:23:04 +0300

    Update ekzamen.cpp

--- ekzamen.cpp
+++ ekzamen.cpp
@@ -1,1 +1,26 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/core/core.hpp>
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include <string>
 
+using namespace cv;
+using namespace std;
+int main()
+{
+	
+	int height = 1080;
+	int width = 1920;
+	Mat img = imread("C:/Users/Администратор/Desktop/ConsoleApplication1/cruzak200.jpg");
+	Point textOrg(100, img.rows / 2);
+	int fontFace = FONT_HERSHEY_SIMPLEX;
+	double fontScale = 3;
+	Scalar color(9, 0, 255);
+	putText(img, "hello world", textOrg, fontFace, fontScale, color);
+	imshow("hello world", img);
+	waitKey(0);
+	return 0;
+
+}
